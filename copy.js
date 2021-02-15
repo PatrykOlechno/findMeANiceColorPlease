9929516eda9235cd5541fa14b1989d8859f1bac1
Patryk Olechno <patrykoolechno@gmail.com>
2021-02-15 18:37:56 +0100

    Added smooth alert about coping color

--- copy.js
+++ copy.js
@@ -1,9 +1,13 @@
 const copy = document.getElementById("actual-color");
 const button = document.getElementById("copy");
+const alert = document.getElementById("alert");
 
 button.onclick = () => {
     copy.select();
     copy.setSelectionRange(0, 99999); //for mobile
     document.execCommand("copy");
-    
+    alert.style.opacity = "1"; 
+    setTimeout(() =>{
+        alert.style.opacity = "0"; 
+    }, 2000);
 }
